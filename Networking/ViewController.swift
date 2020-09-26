421eb093ab50c0decfa863ab5118b2f61cc05ce8
FELIX <felixkautz@gmail.com>
2020-09-26 10:01:58 +0300

    Initial Commit

---
+++ Networking/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Felix Falkovsky on 26.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var getImageButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func getImagePressed(_ sender: Any) {
+    }
+    
+}
+
