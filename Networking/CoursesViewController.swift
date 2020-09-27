ab3be4f1c81e510e2cb157cd9521f9a2d7de30bb
FELIX <felixkautz@gmail.com>
2020-09-27 12:58:32 +0300

    Исправлено Имена констант Snake_Case на CamelCase ( number_of_lessens на NumberOfLessens ) (number_of_tests на namberOfTests) Выведены значения в View Выполнена асинхронная загрузка изображений.

--- Networking/CoursesViewController.swift
+++ Networking/CoursesViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class CoursesViewController: UIViewController {
     
+    private var courses = [Course]()
+    
     @IBOutlet var tabView: UITableViewCell!
     
     override func viewDidLoad() {
@@ -19,18 +21,42 @@ class CoursesViewController: UIViewController {
     
     func fetchData() {
        // let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_course"
-       // let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_courses"
-          let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_website_description"
+        let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_courses"
+       // let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_website_description"
+       //   let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_missing_or_wrong_fields"
         guard let url = URL(string: jsonUrlString) else { return }
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let data = data else { return }
             do {
-                let websiteDescription = try JSONDecoder().decode(WebsiteDescription.self, from: data)
-                print("\(websiteDescription.websiteName)", "\(websiteDescription.websiteDescription)")
+                self.courses = try JSONDecoder().decode([Course].self, from: data)
+                DispatchQueue.main.async {
+                    self.tabView.reloadInputViews()
+                }
             } catch let error {
                 print("*** ERROR JSON ***", error)
             }
         }.resume()
+    }
+    
+    private func configureCell(cell: TableViewCell, for indexPath: IndexPath) {
+        let course = courses[indexPath.row]
+        cell.courseNameLabel.text = course.name
+        
+        if let namberOfLessons = course.numberOfLessons {
+            cell.numberOfLessons.text = "Number of Lessons \(namberOfLessons)"
+        }
+        
+        if let numberOfTests = course.numberOfTests {
+            cell.numberOfTests.text = "Number of Test \(numberOfTests)"
+        }
+        DispatchQueue.global().async {
+            guard let imageUrl = URL(string: course.imageUrl!) else { return }
+            guard let imageData = try? Data(contentsOf: imageUrl) else { return }
+            
+            DispatchQueue.main.async {
+                cell.courseImage.image = UIImage(data: imageData)
+            }
+        }
     }
 }
 
@@ -39,13 +65,14 @@ class CoursesViewController: UIViewController {
 extension CoursesViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        
+        return courses.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! TableViewCell
-        
+        configureCell(cell: cell, for: indexPath)
         return cell
     }
 }
--- Networking/WebsiteDescription.swift
+++ Networking/WebsiteDescription.swift
@@ -9,7 +9,7 @@ import Foundation
 
 
 struct WebsiteDescription: Decodable {
-    let websiteDescription: String
-    let websiteName: String
+    let websiteDescription: String?
+    let websiteName: String?
     let courses: [Course]
 }
